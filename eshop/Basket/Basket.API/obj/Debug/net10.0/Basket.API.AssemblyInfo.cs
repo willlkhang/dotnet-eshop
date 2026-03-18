93ef200ee55077afccd656a4a693215189e58a85
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-18 21:01:06 +0800

    Disocunt: init new project serivce

--- eshop/Basket/Basket.API/obj/Debug/net10.0/Basket.API.AssemblyInfo.cs
+++ eshop/Basket/Basket.API/obj/Debug/net10.0/Basket.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd25321289264d2a329ebefdcabf7c55f9b02ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c38df3dced9b7aeb7617638df0b0875518c760")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
