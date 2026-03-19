d7f147fa49161340b2286a3a941eadbc3720204f
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-19 19:12:56 +0800

    Order: create payment in valueObject

--- eshop/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ eshop/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -2,5 +2,14 @@ namespace Ordering.Domain.ValueObjects;
 
 public class Address
 {
-    
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
+    public string? EmailAddress { get; } = default!;
+    public string AddressLine { get; } = default!;
+    public string Country { get; } = default!;
+    public string State { get; } = default!;
+    public string ZipCode { get; } = default!;
+    protected Address()
+    {
+    }
 }
