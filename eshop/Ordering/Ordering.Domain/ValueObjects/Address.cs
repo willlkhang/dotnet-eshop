980f245129782698a2bc29249767bc20d78fb40d
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-23 12:02:23 +0800

    Order: update address entity

--- eshop/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ eshop/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -12,4 +12,23 @@ public class Address
     protected Address()
     {
     }
+    
+    private Address(string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string zipCode)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        AddressLine = addressLine;
+        Country = country;
+        State = state;
+        ZipCode = zipCode;
+    }
+
+    public static Address Of(string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string zipCode)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+        ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+        return new Address(firstName, lastName, emailAddress, addressLine, country, state, zipCode);
+    }
 }
